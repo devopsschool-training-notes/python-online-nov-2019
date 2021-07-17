ad56c55ab18ffb1b50713a579af2fcc1f3e4216b
DevOps School <44023974+devops-school@users.noreply.github.com>
2021-07-17 22:19:32 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,27 @@
+# Creating a Function
+
+def my_function():
+  print("Hello from a function")
+
+# Calling a Function
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+# Python Function: Arguments
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+# Number of Arguments
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
